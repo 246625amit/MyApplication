83577f6458133927ea9bddd9faad54d59f0827ab
246625amit <246625amit@gmail.com>
2020-04-20 13:31:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    Button logout,clist;
+    TextView contactList;
+    ActionBar actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        logout=findViewById(R.id.btnLogout);
+        clist=findViewById(R.id.messageList);
+        contactList=findViewById(R.id.contactstv);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                startActivity(intent);
+            }
+        });
+
+        clist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ProfileActivity.this,ContactsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
